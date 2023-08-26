8232fbaf2d6a38893ef9545de6a07a32c78a453d
galina <galina.ramodina@gmail.com>
2023-08-26 12:26:10 +0300

    готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[] method (int[] massiv) {
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] % 2 == 0) {
+                massiv[i] ++;
+            }
+        }
+        return Arrays.copyOfRange(massiv, 3, 7);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] massiv = new int[]{2, 3, 1, 6, 4, 8, 9, 5, 13, 7};
+        System.out.println(Arrays.toString(method(massiv)));
     }
 }
