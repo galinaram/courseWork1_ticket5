cfb8cac0a378e8b612b40e6114415f1ed4e06aca
galina <galina.ramodina@gmail.com>
2023-08-28 17:40:15 +0300

    готово

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
                 massiv[i] ++;
             }
         }
+        System.out.println(Arrays.toString(massiv));
         return Arrays.copyOfRange(massiv, 3, 7);
     }
+
     public static void main(String[] args) {
-        int [] massiv = new int[]{2, 3, 1, 6, 4, 8, 9, 5, 13, 7};
+        int[] massiv = new int[]{2, 6, 9 ,4 , 18, 67, 13, 5, 9, 10};
         System.out.println(Arrays.toString(method(massiv)));
     }
 }
